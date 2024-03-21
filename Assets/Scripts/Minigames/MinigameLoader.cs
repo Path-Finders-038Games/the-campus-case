a3c4ccbeff647164ed9370735a029d62a883aeea
Luximus <thomaslipman10@gmail.com>
2024-03-21 13:17:15 +0100

    add comments to the minigame loader

--- Assets/Scripts/Minigames/MinigameLoader.cs
+++ Assets/Scripts/Minigames/MinigameLoader.cs
@@ -9,69 +9,101 @@ namespace Minigames
 {
     public class MinigameLoader : MonoBehaviour
     {
+        /// <summary>
+        /// This class is located here so it's visible in Unity.
+        /// </summary>
         [System.Serializable]
         public class Minigame
         {
             public MinigameName MinigameName;
             public GameObject MinigamePrefab;
         }
-        
-        private GameObject _spawnNew;
 
-        public Minigame[] Minigames;
         public GameObject Default;
+        public Minigame[] Minigames;
 
-        private ARRaycastManager _rayCastManager;
-        private static readonly List<ARRaycastHit> Hits = new();
+        private GameObject _spawnedPrefab;
+
+        private ARRaycastManager _raycastManager;
         private ARRaycastHit _planeHit;
-        private float _distance = 999;
+
+        private static readonly List<ARRaycastHit> PreviousRaycastHits = new();
 
         private void Start()
         {
-            _rayCastManager = GetComponent<ARRaycastManager>();
+            // Get the ARRaycastManager from the ARSessionOrigin.
+            // This should be set in the Unity Editor, but it can't be since the prefab is not discoverable.
+            _raycastManager = GetComponent<ARRaycastManager>();
         }
 
         private void Update()
         {
+            // If the prefab is already spawned, return.
+            if (_spawnedPrefab != null) return;
+
+            // Check if there is more than 0 touches and if the touch is the beginning
+            // of the touch (to prevent multiple touches at once and holding down).
             if (Input.touchCount <= 0 || Input.touches[0].phase != TouchPhase.Began) return;
 
-            _distance = 999;
+            // Set the distance to the maximum value. This is used to find the closest plane.
+            // This value is decreased when a closer plane is found.
+            float distance = float.MaxValue;
 
-            if (!_rayCastManager.Raycast(Input.touches[0].position, Hits,
+            // If the raycast does not hit any planes, return. Planes are the dotted surfaces
+            // visible in the AR environment.
+            if (!_raycastManager.Raycast(Input.touches[0].position, PreviousRaycastHits,
                     TrackableType.Planes)) return;
-            
-            foreach (ARRaycastHit plane in Hits.Where(plane => plane.distance < _distance))
+
+            // Find the closest plane. This is done by iterating through all the planes and
+            // checking if the distance is smaller than the previous distance.
+            foreach (ARRaycastHit plane in PreviousRaycastHits)
             {
-                _distance = plane.distance;
+                // If the current plane is not closer than the previous plane, exit the loop.
+                if (!(plane.distance < distance)) continue;
+
+                // Set the distance to the current plane's distance, and set the planeHit to the current plane.
+                distance = plane.distance;
                 _planeHit = plane;
             }
 
-            if (_spawnNew == null)
-            {
-                SpawnPrefab();
-            }
+            // Spawn the prefab at the hit position.
+            SpawnPrefab(_planeHit);
         }
 
-        private void SpawnPrefab()
+        private void SpawnPrefab(ARRaycastHit plane)
         {
-            Quaternion temp = _planeHit.pose.rotation;
-            //To have the placeable objects in the right rotation -90 is used
-            temp.eulerAngles = new Vector3(GetCorrectPrefab().transform.eulerAngles.x, temp.eulerAngles.y,
-                temp.eulerAngles.z);
-            _spawnNew = Instantiate(GetCorrectPrefab(), _planeHit.pose.position, temp);
+            // Get the rotation of the hit plane.
+            Quaternion rotation = plane.pose.rotation;
+
+            // The prefab is rotated -90 degrees to have the correct rotation relative to the plane.
+            rotation.eulerAngles = new Vector3(GetCorrectPrefab().transform.eulerAngles.x, rotation.eulerAngles.y,
+                rotation.eulerAngles.z);
+
+            // Instantiate the prefab at the hit position with the correct rotation.
+            _spawnedPrefab = Instantiate(GetCorrectPrefab(), plane.pose.position, rotation);
         }
 
         private GameObject GetCorrectPrefab()
         {
-            if (!Minigames.Select(e => SceneLoader.GetMinigameMapName(e.MinigameName)).Contains(DataManager.Instance.CurrentMap)) return Default;
+            // Check if the current map is not a minigame map.
+            // If it's not, return the default prefab.
+            if (!Minigames.Select(e => SceneLoader.GetMinigameMapName(e.MinigameName))
+                    .Contains(DataManager.Instance.CurrentMap)) return Default;
 
-            return Minigames.Where(e => SceneLoader.GetMinigameMapName(e.MinigameName) == DataManager.Instance.CurrentMap).Select(e => e.MinigamePrefab)
+            // Return the minigame prefab that matches the current map.
+            return Minigames
+                .Where(e => SceneLoader.GetMinigameMapName(e.MinigameName) == DataManager.Instance.CurrentMap)
+                .Select(e => e.MinigamePrefab)
                 .First();
         }
 
         public void DestroyPrefab()
         {
-            Destroy(_spawnNew);
+            // If the prefab is not spawned, return.
+            if (_spawnedPrefab == null) return;
+
+            // Destroy the prefab.
+            Destroy(_spawnedPrefab);
         }
     }
 }
